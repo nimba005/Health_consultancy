44a2305135d449d1115c2798fc70a30cffafc7cd
nimba005 <nimbaoyugi@gmail.com>
2024-08-08 00:46:28 +0300

    Added Consultation component that renders a page displaying a list of consultations

---
+++ frontend/src/pages/Consultations.tsx
@@ -0,0 +1,12 @@
+import ConsultationsTable from "../components/Consultations/ConsultationsTables"
+
+const Consultations = () => {
+    return (
+        <div>
+            <h1 className=" text-3xl font-bold mb-8">List of Consultations</h1>
+            <ConsultationsTable />
+        </div>
+    )
+}
+
+export default Consultations
