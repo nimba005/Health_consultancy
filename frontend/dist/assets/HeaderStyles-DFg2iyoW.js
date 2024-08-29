6e09d2a944c74d483faa60a8a94dfea9d759b33c
nimba005 <nimbaoyugi@gmail.com>
2024-08-29 23:22:51 +0300

    update

---
+++ frontend/dist/assets/HeaderStyles-DFg2iyoW.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-H8nQwn4j.js";const s=({header_title:t})=>e.jsx("h1",{className:" text-3xl font-bold text-center",children:t});export{s as H};
---
+++ frontend/dist/assets/SurfaceUI-CUuDGGy_.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-H8nQwn4j.js";const t=({children:s})=>e.jsx("div",{className:" flex items-center justify-center min-h-screen w-full",children:e.jsx("div",{className:" bg-light-gray p-10 my-10 rounded-xl w-[50%] max-md:w-[90%]",children:s})});export{t as S};
