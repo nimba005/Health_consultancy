1e24c1b2e1e9901cb1253a77140fda0fe9736c53
nimba005 <nimbaoyugi@gmail.com>
2024-09-04 17:53:15 +0300

    updsted the registry components

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,25 +1,33 @@
-import dotenv from "dotenv";
-dotenv.config(); // Load environment variables first
-
 import express from "express";
-import { HttpError } from "http-errors";
-import cookieParser from "cookie-parser";
 import cors from "cors";
+import dotenv from "dotenv";
 import logger from "morgan";
 import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 import { database } from "./config";
 import userRoutes from "./routes/userRoute/userRoutes";
 import patientRoutes from "./routes/patientRoute/patientRoutes";
 import consultantRoute from "./routes/ConsultantRoute/consultantRoute";
 
+dotenv.config(); // Load environment variables
+
 const app = express();
+
+// CORS configuration
+const allowedOrigins = ["http://localhost:5173", "http://localhost:3000"];
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true, // Allow cookies to be sent/received
+  })
+);
 
 app.use(bodyParser.json());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+
 app.use("/users", userRoutes);
 app.use("/patient", patientRoutes);
 app.use("/consultations", consultantRoute);
@@ -29,12 +37,12 @@ database
   .then(async () => {
     console.log("Database is connected");
   })
-  .catch((err: HttpError) => {
+  .catch((err: Error) => {
     console.log(err);
   });
 
 app.listen(process.env.PORT, () => {
-  console.log(`server running on ${process.env.PORT}`);
+  console.log(`Server running on ${process.env.PORT}`);
 });
 
 export default app;
